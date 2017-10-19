f2512f8c256fc3ef947da98c691f795d28055262
Vijayalakshmi <vijayalakshmi@con.acad.1>
2017-10-19 12:56:16 +0530

    more changes to resource proposal

--- contracts/ResourceProposal.sol
+++ contracts/ResourceProposal.sol
@@ -1,28 +1,33 @@
 pragma solidity ^0.4.6;
 
 import "./Stoppable.sol";
+import "./Hub.sol";
 
-contract ResourceProposal is Stoppable, Hub {
+contract ResourceProposal is Stoppable {
 
 	int chairmanFee;
 	int deadline;
 	address chairman;
 	int projectCost;
-	string proposalText;
+	bytes32 proposalText;
 	bool isDependent;
 	address depParent;
 
 	mapping(address => uint8) votes;
-	//
+	//votes 0 is don't care, 1 yes, 2 no
 	mapping(address => string) opinions;
 	
+	modifier onlyIfMember(address a) {
+		//require(isMember(a));
+		_;
+	}
 
-	event LogProposalCreated(address owner, address chairmanAddress, int fees, int blocks, int cost, string text);
+	event LogProposalCreated(address owner, address chairmanAddress, int fees, int blocks, int cost, bytes32 text);
 	event LogVoteCast(address member, uint8 vote);
 	event LogProposalSentToHub(address owner, uint blockNumber);
 	event LogOpinionAdded(address member, bytes32 opinion);
 
-	function ResourceProposal(address chairmanAddress, int fees, int blocks, int cost, string text)
+	function ResourceProposal(address chairmanAddress, int fees, int blocks, int cost, bytes32 text)
 	{
 		chairman = chairmanAddress;
 		chairmanFee = fees;
@@ -32,10 +37,55 @@ contract ResourceProposal is Stoppable, Hub {
 		LogProposalCreated(owner, chairmanAddress, fees, blocks, cost, text);
 	}
 
-	function castVote(uint8 vote)
-	public
-	returns(bool)
+	function getChairman()
+		public
+		constant
+		returns(address)
 	{
+		return chairman;
+	}
+	
+	function status()
+		public
+		constant
+		returns(uint8)
+	{
+		//if(block.number > deadline) return DEADLINE_PASSED;
+		//TODO: check for various conditions;
+		return 2;
 
+	}
+
+
+	function castVote(uint8 voteOfMember)
+		public
+		onlyIfMember(msg.sender)
+		returns(bool)
+	{
+		require(vote[msg.sender]==0);
+
+		vote[msg.sender] = voteOfMember;
+		
+		LogVoteCast(msg.sender, voteOfMember);
+		return true;
+	}
+
+	function giveOpinion(bytes32 opinion)
+		public
+		onlyIfMember(msg.sender)
+		returns(bool)
+	{
+		opinions[msg.sender] = opinion;
+
+		LogOpinionAdded(msg.sender, opinion);
+		return true;
+	}
+
+
+	function sendToHub()
+		public
+		returns(bool)
+	{
+		
 	}
 }
