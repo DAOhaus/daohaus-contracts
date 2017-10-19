f2abc3afc683267737807a21c105691d7fd46be6
Nikhil Bhaskar <nikhil38@gmail.com>
2017-10-19 11:01:29 +0400

    add untested daohaus hub contract

--- contracts/Stoppable.sol
+++ contracts/Stoppable.sol
@@ -1,6 +1,6 @@
 //////////////////////////////////////////////////////////
 // For training purposes.
-// Solidity Contract Factory 
+// Solidity Contract Factory
 // Module 5 - START
 // Copyright (c) 2017, Rob Hitchens, all rights reserved.
 // Not suitable for actual use
@@ -11,20 +11,20 @@ pragma solidity ^0.4.6;
 import "./Owned.sol";
 
 contract Stoppable is Owned {
-    
+
     bool public running;
-    
+
     event LogRunSwitch(address sender, bool switchSetting);
-        
-    modifier onlyIfRunning { 
-        if(!running) throw; 
+
+    modifier onlyIfRunning {
+        require(running); 
         _;
     }
-        
+
     function Stoppable() {
         running = true;
     }
-        
+
     function runSwitch(bool onOff)
         onlyOwner
         returns(bool success)
@@ -33,5 +33,5 @@ contract Stoppable is Owned {
         LogRunSwitch(msg.sender, onOff);
         return true;
     }
-        
+
 }
