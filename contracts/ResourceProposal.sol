7a05ccf668717afd3ac25361a7b301951798b95d
Vijayalakshmi <vijayalakshmi@con.acad.1>
2017-10-19 11:49:44 +0530

    Basic resource proposal contract

---
+++ contracts/ResourceProposal.sol
@@ -0,0 +1,41 @@
+pragma solidity ^0.4.6;
+
+import "./Stoppable.sol";
+
+contract ResourceProposal is Stoppable, Hub {
+
+	int chairmanFee;
+	int deadline;
+	address chairman;
+	int projectCost;
+	string proposalText;
+	bool isDependent;
+	address depParent;
+
+	mapping(address => uint8) votes;
+	//
+	mapping(address => string) opinions;
+	
+
+	event LogProposalCreated(address owner, address chairmanAddress, int fees, int blocks, int cost, string text);
+	event LogVoteCast(address member, uint8 vote);
+	event LogProposalSentToHub(address owner, uint blockNumber);
+	event LogOpinionAdded(address member, bytes32 opinion);
+
+	function ResourceProposal(address chairmanAddress, int fees, int blocks, int cost, string text)
+	{
+		chairman = chairmanAddress;
+		chairmanFee = fees;
+		deadline = block.number + blocks;
+		projectCost = cost;
+		proposalText = text;
+		LogProposalCreated(owner, chairmanAddress, fees, blocks, cost, text);
+	}
+
+	function castVote(uint8 vote)
+	public
+	returns(bool)
+	{
+
+	}
+}
