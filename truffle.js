aa0e0f614d9c3a3b1db958dd3950f1a20f3551df
Nikhil Bhaskar <nikhil38@gmail.com>
2017-10-20 12:52:21 +0400

    deploy contract in ropsten

--- truffle.js
+++ truffle.js
@@ -1,13 +1,17 @@
+const constants = require('./constants');
+const HDWalletProvider = require("truffle-hdwallet-provider");
+
 module.exports = {
   networks: {
     development: {
       host: "localhost",
       port: 8545,
       network_id: "*" // Match any network id
+    },
+    ropsten: {
+      provider: new HDWalletProvider(constants.mnemonic, "https://ropsten.infura.io/" + constants.infura_apikey),
+      network_id: 3,
+      gas:   2900000
     }
-  },
-  compiler: {
-   version: "0.4.15",
-   optimization: false
- }
+  }
 };
